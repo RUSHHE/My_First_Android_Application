0b7a7e20c27bf8dd31e2e17092ee25509f885f56
RUSHHE <rushhe@zjxu.edu.cn>
2024-03-13 13:50:28 +0800

    ViewPager2加TabLayout实现 最初版朋友圈页（Experimental）

--- app/src/main/java/com/example/myapplication/BlankFragment.java
+++ app/src/main/java/com/example/myapplication/BlankFragment.java
@@ -1,63 +1,112 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BlankFragment#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
-public class BlankFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public BlankFragment() {
-        // Required empty public constructor
+public class BlankFragment extends Fragment implements AdapterView.OnItemClickListener {
+    String[] name = {"Tom", "Jack", "Mike", "Tom", "Jack", "Mike", "Tom", "Jack", "Mike", "Tom", "Jack", "Mike"};
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        return view;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BlankFragment.
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ListView listView = view.findViewById(R.id.VeChatList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, name);
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        Toast.makeText(getActivity(), name[position], Toast.LENGTH_SHORT).show();
+    }
+
+    /*
+     * 以下是生命周期方法
      */
-    // TODO: Rename and change types and number of parameters
-    public static BlankFragment newInstance(String param1, String param2) {
-        BlankFragment fragment = new BlankFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        Log.d("BlankFragment", "onCreate() called");
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.d("BlankFragment", "onAttach() called");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d("BlankFragment", "onDetach() called");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d("BlankFragment", "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d("BlankFragment", "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d("BlankFragment", "onPause() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d("BlankFragment", "onStop() called");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d("BlankFragment", "onDestroyView() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("BlankFragment", "onDestroy() called");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d("BlankFragment", "onSaveInstanceState() called");
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        Log.d("BlankFragment", "onViewStateRestored() called");
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,39 +1,49 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.app.Dialog;
 import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.example.myapplication.databinding.DialogCustomBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ImageView imageView=findViewById(R.id.imageView);
+        ActivityMainBinding amBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(amBinding.getRoot());
 
         Glide.with(this)
                 .load("https://avatars.githubusercontent.com/u/63961439?s=400&u=7f18725e8c4eddf8cdf26e9d438e16f3760e13ab&v=4")
-                .into(imageView);
+                .into(amBinding.imageView);
 
-        TextView accountText = findViewById(R.id.accountText);
-        Button loginButton = findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(v -> {
-            Toast.makeText(this, "按钮已按下" + "帐号:" + accountText.getText(), Toast.LENGTH_SHORT).show();
+        amBinding.loginButton.setOnClickListener(v -> {
+            Toast.makeText(this, "按钮已按下" + "帐号:" + amBinding.accountText.getText(), Toast.LENGTH_SHORT).show();
             ComponentName componentName = new ComponentName(this, "com.example.myapplication.MainActivity2");
             startActivity(new Intent().setComponent(componentName));
         });
 
 
+        Dialog dialog = new Dialog(this);
+        DialogCustomBinding dcBinding = DialogCustomBinding.inflate(getLayoutInflater());
+        dialog.setContentView(dcBinding.getRoot());
+        Glide.with(this).load("https://img.moehu.org/pic.php?id=miku")
+                .into(dcBinding.dialogImageView);
+//        dialog.show();
     }
 }
