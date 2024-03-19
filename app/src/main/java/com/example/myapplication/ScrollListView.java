9ae3a668b70f14211ddf867c784b2b60fc335613
RUSHHE <rushhe@zjxu.edu.cn>
2024-03-19 16:49:41 +0800

    更新了消息界面的样式，使用NestedScrollView实现了搜索栏跟着列表走。

---
+++ app/src/main/java/com/example/myapplication/ScrollListView.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+public class ScrollListView extends ListView {
+
+    public ScrollListView(Context context) {
+        super(context);
+    }
+
+    public ScrollListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ScrollListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    /*
+      重写onMeasure方法，设置ListView的高度为最大值
+     */
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+}
